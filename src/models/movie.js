cabb18c88464f837c494a97b7bd7e1685b135f9f
Jordan Welch <jordanhwelch@gmail.com>
2023-05-17 18:01:27 -0500

    Assign all fields.

--- src/models/movie.js
+++ src/models/movie.js
@@ -1,15 +1,23 @@
 export class Movie {
-  constructor (id, title, year) {
+  constructor (id, title, year, length, imdbUrl, posterUrl) {
     this.id = id;
     this.title = title;
     this.year = year;
+    this.length = length;
+    this.imdbUrl = imdbUrl;
+    this.posterUrl = posterUrl;
   }
 
   static fromNotion (movie) {
+    console.log(movie.properties);
+
     return new Movie(
       movie.id,
-      movie.properties['Title'].title[0].plain_text,
-      movie.properties['Year'].number
+      movie.properties['Title']?.title[0]?.plain_text,
+      movie.properties['Year']?.number,
+      movie.properties['Length (mins)']?.number,
+      movie.properties['IMDb']?.url,
+      movie.properties['Poster']?.url,
     );
   }
 
