43eee6f011e854fc0267b53f40c36c566926ea9d
Jordan Welch <jordanhwelch@gmail.com>
2023-05-17 21:57:15 -0500

    Add deploy scripts

---
+++ scripts/deploy.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+echo '****************************************'
+echo '***         Building Assets          ***'
+echo '****************************************'
+npm run build
+
+if [ -f .env ]; then
+    echo '****************************************'
+    echo '***        Backing Up .env           ***'
+    echo '****************************************'
+
+    mv .env .env.temp
+    MOVED_ENV=true
+fi
+
+echo '****************************************'
+echo '***        Production .env           ***'
+echo '****************************************'
+cp .env.production .env
+
+echo '****************************************'
+echo '***         Deploying App            ***'
+echo '****************************************'
+gcloud app deploy
+
+if [ "$MOVED_ENV" = true ]; then
+    echo '****************************************'
+    echo '***        Restoring .env            ***'
+    echo '****************************************'
+
+    mv .env.temp .env
+fi
