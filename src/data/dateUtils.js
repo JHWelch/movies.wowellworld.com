58276335a77167bdad912abc247456a18aa1657c
Jordan Welch <jordanhwelch@gmail.com>
2023-07-02 14:27:32 -0500

    Remove uneeded functions

--- src/data/dateUtils.js
+++ src/data/dateUtils.js
@@ -6,31 +6,6 @@ export default class DateUtils {
     return DateUtils.dateToString(today);
   }
 
-  static getThursdayDate() {
-    const today = new Date();
-
-    const thursday = today.getDay() < 5
-      ? new Date(today.setDate(today.getDate() - today.getDay() + 4))
-      : new Date(today.setDate(today.getDate() + 11 - today.getDay()));
-
-    thursday.setHours(0, 0, 0, 0);
-
-    return thursday;
-  }
-
-  static getThursday() {
-    return DateUtils.dateToString(this.getThursdayDate());
-  }
-
-  static getNextTwoThursdays() {
-    const thursday = this.getThursdayDate();
-
-    return [
-      this.dateToString(new Date(thursday.setDate(thursday.getDate() + 7))),
-      this.dateToString(new Date(thursday.setDate(thursday.getDate() + 7))),
-    ];
-  }
-
   static dateToString(date) {
     return date.toISOString().substring(0, 10);
   }
