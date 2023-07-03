a126bd87017f8be633ae5172b68320deea17ff21
Jordan Welch <jordanhwelch@gmail.com>
2023-07-02 21:33:09 -0500

    WIP

--- src/index.js
+++ src/index.js
@@ -5,11 +5,8 @@ import Notion from './data/notion.js';
 
 dotenv.config();
 
-console.log(process.env.NODE_ENV);
-console.log(process.env.CALENDAR_URL);
+const app = process.env.NODE_ENV === 'development'
+  ? new FakeApplication()
+  : new Application(new Notion());
 
-// const app = process.env.NODE_ENV === 'development'
-//   ? new FakeApplication()
-//   : new Application(new Notion());
-
-// app.listen();
+app.listen();
