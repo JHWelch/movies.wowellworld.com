030bacc8678d9fc81ae5d96870a00f0c26af98c1
Jordan Welch <jordanhwelch@gmail.com>
2023-07-04 14:36:05 -0500

    Add week query mocking

--- __mocks__/@notionhq/client.ts
+++ __mocks__/@notionhq/client.ts
@@ -1,9 +1,11 @@
 import { jest } from '@jest/globals'
 
-const title = (title: string) => ({ title: [{ plain_text: title }] })
-const richText = (text: string) => ({ rich_text: [{ plain_text: text }] })
-const url = (url: string) => ({ url })
-const number = (number: number) => ({ number })
+const nCheckbox = (checked: boolean) => ({ checkbox: checked })
+const nDate = (start: string) => ({ date: { start } })
+const nNumber = (number: number) => ({ number })
+const nRichText = (text: string) => ({ rich_text: [{ plain_text: text }] })
+const nTitle = (title: string) => ({ title: [{ plain_text: title }] })
+const nUrl = (url: string) => ({ url })
 
 module.exports = {
   Client: jest.fn().mockImplementation(() => {
@@ -19,20 +21,77 @@ module.exports = {
           return {
             id: 'movieId',
             properties: {
-              Title: title('movieTitle'),
-              Director: richText('movieDirector'),
-              Year: number(2021),
-              'Length (mins)': number(120),
-              IMDb: url('movieImdbUrl'),
-              Poster: url('moviePosterUrl'),
-              'Theater Name': richText('movieTheaterName'),
-              'Showing URL': url('movieShowingUrl'),
+              Title: nTitle('movieTitle'),
+              Director: nRichText('movieDirector'),
+              Year: nNumber(2021),
+              'Length (mins)': nNumber(120),
+              IMDb: nUrl('movieImdbUrl'),
+              Poster: nUrl('moviePosterUrl'),
+              'Theater Name': nRichText('movieTheaterName'),
+              'Showing URL': nUrl('movieShowingUrl'),
             },
           }
+        }),
+      },
+
+      databases: {
+        query: jest.fn().mockImplementation((query: unknown) => {
+          const { database_id, filter } = query as QueryBody
+
+          if (database_id !== 'DATABASE_ID') {
+            throw new Error('Database not found')
+          }
+          if (filter == null){
+            throw new Error('Filter not specified')
+          }
+
+          const { property, date, sorts } = filter
+
+          if (property !== 'Date') {
+            throw new Error('Invalid property')
+          }
+          if (date == null) {
+            throw new Error('Date not specified')
+          }
+
+          const { equals, on_or_after } = date
+
+          if (equals !== undefined) {
+            return { results: [{
+              id: 'weekId',
+              properties: {
+                Date: nDate(equals),
+                Theme: nTitle('weekTheme'),
+                Skipped: nCheckbox(false),
+                Movies: {
+                  relation: [
+                    // { id: 'movieId' },
+                  ],
+                },
+              },
+            }]}
+          }
+
         }),
       },
     }
   }),
 
   isFullPage: jest.fn(),
+}
+
+type QueryBody = {
+  database_id: string
+  page_size?: number
+  filter?: {
+    property: string
+    date?: {
+      equals?: string
+      on_or_after?: string
+    }
+    sorts?: {
+      property: string
+      direction: string
+    }[]
+  }
 }
