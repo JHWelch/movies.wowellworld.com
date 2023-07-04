4aaf38b61cf57c347b8b6908e5925a89570813c8
Jordan Welch <jordanhwelch@gmail.com>
2023-07-04 09:52:49 -0500

    Get Jest working fully

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  transform: {
+    '\\.[jt]sx?$': ['ts-jest', {
+      useESM: true,
+    }],
+  },
+  moduleNameMapper: {
+    '(.+)\\.js': '$1',
+  },
+  extensionsToTreatAsEsm: ['.ts'],
   preset: 'ts-jest',
   testEnvironment: 'node',
 }
