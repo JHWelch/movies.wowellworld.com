5a7749558bd1e44d8249b6c48204e956c48f3951
Jordan Welch <jordanhwelch@gmail.com>
2023-07-03 23:03:09 -0500

    More different not working

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,25 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": "standard-with-typescript",
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module",
-        "project": "./tsconfig.json"
-    },
-    "rules": {
-    }
+  'root': true,
+  'parser': '@typescript-eslint/parser',
+  'env': {
+    'browser': true,
+    'es2022': true
+  },
+  'extends': 'standard-with-typescript',
+  'overrides': [],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+    'project': './tsconfig.json'
+  },
+  'rules': {
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/keyword-spacing': 'off',
+    '@typescript-eslint/semi': 'off',
+  },
+  'ignorePatterns': [
+    'built/**/*',
+    'node_modules/**/*',
+    '/tailwind.config.js',
+  ]
 }
