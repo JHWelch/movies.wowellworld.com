4b1fda3b0c60a8ad2e4111c29ed291affe4a8c70
Jordan Welch <jordanhwelch@gmail.com>
2023-07-04 10:01:50 -0500

    Fully test constructor

--- __tests__/data/notion.spec.ts
+++ __tests__/data/notion.spec.ts
@@ -11,14 +11,46 @@ beforeEach(() => {
 
 describe('notion', () => {
   describe('constructor', () => {
-    it('should set the database id', () => {
-      process.env = {
-        NOTION_TOKEN: 'NOTION_TOKEN',
-        DATABASE_ID: 'DATABASE_ID',
-      }
-      const notion = new Notion()
+    describe('when NOTION_TOKEN and DATABASE_ID are set', () => {
+      beforeEach(() => {
+        process.env = {
+          NOTION_TOKEN: 'NOTION_TOKEN',
+          DATABASE_ID: 'DATABASE_ID',
+        }
+      })
 
-      expect(notion._databaseId).toEqual('DATABASE_ID')
+      it('should set the database id', () => {
+        expect(new Notion()._databaseId).toEqual('DATABASE_ID')
+      })
+
+      it('should set the notion client', () => {
+        expect(new Notion().notion).toBeDefined()
+      })
+    })
+
+
+    describe('when NOTION_TOKEN is not set', () => {
+      beforeEach(() => {
+        process.env = {
+          DATABASE_ID: 'DATABASE_ID',
+        }
+      })
+
+      it('should throw an error', () => {
+        expect(() => new Notion()).toThrowError('Missing NOTION_TOKEN environment variable')
+      })
+    })
+
+    describe('when DATABASE_ID is not set', () => {
+      beforeEach(() => {
+        process.env = {
+          NOTION_TOKEN: 'NOTION_TOKEN',
+        }
+      })
+
+      it('should throw an error', () => {
+        expect(() => new Notion()).toThrowError('Missing DATABASE_ID environment variable')
+      })
     })
   })
 })
