7ebdc7924f1fd2bb3e2f9323299036714daf1898
Jordan Welch <jordanhwelch@gmail.com>
2023-07-03 22:23:14 -0500

    Remove "fake" architecture

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import * as dotenv from 'dotenv'
 import Application from './application'
-// import FakeApplication from './dev/fakeApp'
 import Notion from './data/notion'
 
 dotenv.config()
-
-// Disabled for TS
-// const app = process.env.NODE_ENV === 'development'
-//   ? new FakeApplication()
-//   : new Application(new Notion())
 
 const app = new Application(new Notion())
 
