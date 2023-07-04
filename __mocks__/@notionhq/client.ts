e0d4ca9a3558d216946539de07bf3ce5adc7626c
Jordan Welch <jordanhwelch@gmail.com>
2023-07-04 13:19:50 -0500

    Invert function

--- __mocks__/@notionhq/client.ts
+++ __mocks__/@notionhq/client.ts
@@ -7,44 +7,44 @@ module.exports = {
         retrieve: jest.fn().mockImplementation((id: unknown) => {
           const { page_id } = id as { page_id: string }
 
-          if (page_id === 'movieId') {
-            return {
-              id: 'movieId',
-              properties: {
-                Title: {
-                  title: [{
-                    plain_text: 'movieTitle',
-                  }],
-                },
-                Director: {
-                  rich_text: [{
-                    plain_text: 'movieDirector',
-                  }],
-                },
-                Year: {
-                  number: 2021,
-                },
-                'Length (mins)': {
-                  number: 120,
-                },
-                IMDb: {
-                  url: 'movieImdbUrl',
-                },
-                Poster: {
-                  url: 'moviePosterUrl',
-                },
-                'Theater Name': {
-                  rich_text: [{
-                    plain_text: 'movieTheaterName',
-                  }],
-                },
-                'Showing URL': {
-                  url: 'movieShowingUrl',
-                },
-              },
-            }
-          } else {
+          if (page_id !== 'movieId') {
             throw new Error('Page not found')
+          }
+
+          return {
+            id: 'movieId',
+            properties: {
+              Title: {
+                title: [{
+                  plain_text: 'movieTitle',
+                }],
+              },
+              Director: {
+                rich_text: [{
+                  plain_text: 'movieDirector',
+                }],
+              },
+              Year: {
+                number: 2021,
+              },
+              'Length (mins)': {
+                number: 120,
+              },
+              IMDb: {
+                url: 'movieImdbUrl',
+              },
+              Poster: {
+                url: 'moviePosterUrl',
+              },
+              'Theater Name': {
+                rich_text: [{
+                  plain_text: 'movieTheaterName',
+                }],
+              },
+              'Showing URL': {
+                url: 'movieShowingUrl',
+              },
+            },
           }
         }),
       },
