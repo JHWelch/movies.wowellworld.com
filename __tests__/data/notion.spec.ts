8c17d62a9a8574ca85664efc8d4bfbde3461cc1a
Jordan Welch <jordanhwelch@gmail.com>
2023-07-07 19:04:11 -0500

    Add getPastWeeks

--- __tests__/data/notion.spec.ts
+++ __tests__/data/notion.spec.ts
@@ -192,15 +192,69 @@ describe('getUpcomingWeeks', () => {
         page_size: 10,
         filter: {
           property: 'Date',
-          date: {
-            on_or_after: today(),
-          },
+          date: { on_or_after: today() },
         },
         sorts: [{
           property: 'Date',
           direction: 'ascending',
         }],
       })
+    })
+  })
+})
+
+describe ('getPastWeeks', () => {
+  beforeEach(() => {
+    notionMock.mockIsFullPage(true)
+    notionMock.mockQuery([
+      NotionMock.mockWeek('weekId3','2021-01-15', 'theme3'),
+      NotionMock.mockWeek('weekId2','2021-01-08', 'theme2'),
+      NotionMock.mockWeek('weekId1','2021-01-01', 'theme1'),
+    ])
+  })
+
+  it('should return the weeks', async () => {
+    const notion = new Notion()
+    const weeks = await notion.getPastWeeks()
+
+    expect(weeks).toEqual([
+      {
+        'id': 'weekId3',
+        'date': new Date('2021-01-15'),
+        'isSkipped': false,
+        'movies': [],
+        'theme': 'theme3',
+      }, {
+        'id': 'weekId2',
+        'date': new Date('2021-01-08'),
+        'isSkipped': false,
+        'movies': [],
+        'theme': 'theme2',
+      }, {
+        'id': 'weekId1',
+        'date': new Date('2021-01-01'),
+        'isSkipped': false,
+        'movies': [],
+        'theme': 'theme1',
+      },
+    ])
+  })
+
+  it('should call query with the correct parameters', async () => {
+    const notion = new Notion()
+    await notion.getPastWeeks()
+
+    expect(notionMock.query).toHaveBeenCalledWith({
+      database_id: 'DATABASE_ID',
+      page_size: 10,
+      filter: {
+        property: 'Date',
+        date: { before: today() },
+      },
+      sorts: [{
+        property: 'Date',
+        direction: 'descending',
+      }],
     })
   })
 })
