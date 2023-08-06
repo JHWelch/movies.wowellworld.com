2bd932ee227c634a5817de648bbe7c029c6dcf6e
Jordan Welch <jordanhwelch@gmail.com>
2023-08-06 09:33:58 -0500

    Test calling set

--- __mocks__/firebase/firestore.ts
+++ __mocks__/firebase/firestore.ts
@@ -1,20 +1,17 @@
 import { jest } from '@jest/globals'
 
-module.exports = {
-  getFirestore: jest.fn().mockReturnValue({}),
+export const transaction = {
+  set: jest.fn(),
+  get: jest.fn(),
+  update: jest.fn(),
+  delete: jest.fn(),
 }
 
-// jest.mock('firebase/firestore', () => ({
-//   getFirestore: jest.fn().mockReturnValue({}),
-//   doc: jest.fn().mockReturnValue({}),
-//   runTransaction: (firestore: Firestore, updateFunction: (transaction: Transaction) => Promise<void>, options?: TransactionOptions): Promise<void> => {
-//     const transaction = {
-//       set: jest.fn(),
-//       get: jest.fn(),
-//       update: jest.fn(),
-//       delete: jest.fn(),
-//     }
-
-//     return updateFunction(transaction)
-//   },
-// }))
+module.exports = {
+  getFirestore: jest.fn().mockReturnValue({}),
+  runTransaction: (firestore: any, updateFunction: any, _options: any) => {
+    return updateFunction(transaction)
+  },
+  doc: jest.fn().mockReturnValue({}),
+  transaction: transaction,
+}
--- __tests__/controllers/cacheController.spec.ts
+++ __tests__/controllers/cacheController.spec.ts
@@ -6,7 +6,9 @@ import CacheController from '../../src/controllers/cacheController'
 import { initializeApp } from 'firebase/app'
 import { applicationDefault } from 'firebase-admin/app'
 import { doc, getFirestore, Firestore, runTransaction, Transaction } from 'firebase/firestore'
+import { transaction } from '../../__mocks__/firebase/firestore'
 import { Request } from 'express'
+import Week from '../../src/models/week'
 
 
 let notionMock: NotionMock
@@ -36,5 +38,46 @@ describe('constructor', () => {
     expect (initializeApp).toHaveBeenCalledTimes(1)
     expect (getFirestore).toHaveBeenCalledTimes(1)
     expect(cacheController.db).toBeDefined()
+  })
+})
+
+describe('cache', () => {
+  let notion: Notion
+  let req: Request
+
+  beforeEach(() => {
+    notionMock.mockIsFullPageOrDatabase(true)
+    notionMock.mockQuery([
+      NotionMock.mockWeek('id1', '2021-01-01', 'theme1'),
+      NotionMock.mockWeek('id2', '2021-01-08', 'theme2'),
+      NotionMock.mockWeek('id3', '2021-01-15', 'theme3'),
+    ])
+    notion = new Notion()
+    req = getMockReq()
+  })
+
+  describe('when the cache is empty', () => {
+    it('updates all weeks in firestore', async () =>  {
+      const cacheController = new CacheController(notion)
+
+      await cacheController.cache(req, res)
+
+      expect(res.sendStatus).toHaveBeenCalledWith(200)
+      expect(transaction.set)
+        .toHaveBeenCalledWith(
+          doc(cacheController.db, 'weeks', '2021-01-01'),
+          (new Week('id1', 'theme1', new Date('2021-01-01'), false)).toDTO()
+        )
+      expect(transaction.set)
+        .toHaveBeenCalledWith(
+          doc(cacheController.db, 'weeks', '2021-01-08'),
+          (new Week('id2', 'theme2', new Date('2021-01-08'), false)).toDTO()
+        )
+      expect(transaction.set)
+        .toHaveBeenCalledWith(
+          doc(cacheController.db, 'weeks', '2021-01-15'),
+          (new Week('id3', 'theme3', new Date('2021-01-15'), false)).toDTO()
+        )
+    })
   })
 })
