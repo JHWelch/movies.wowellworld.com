a5994a119366ffa97f44b6dd2a25d9da543c2936
Jordan Welch <jordanhwelch@gmail.com>
2023-08-09 22:48:45 -0500

    Add tmdb & inject all from index

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,16 @@ import * as dotenv from 'dotenv'
 import Application from './application.js'
 import NotionAdapter from './data/notionAdapter.js'
 import FirestoreAdapter from './data/firestoreAdapter.js'
+import setupExpress from './config/express.js'
+import TmdbAdapter from './data/tmdb/tmdbAdapter.js'
 
 dotenv.config()
 
 const app = new Application(
+  setupExpress(),
   new FirestoreAdapter(),
-  new NotionAdapter()
+  new NotionAdapter(),
+  new TmdbAdapter(),
 )
 
 app.listen()
