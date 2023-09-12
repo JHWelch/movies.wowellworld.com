76bb87d76a7021e770a4beb0ee2095f51bdcd204
Jordan Welch <jordanhwelch@gmail.com>
2023-09-11 21:40:01 -0500

    'sept' -> 'sep'

--- src/controllers/redirectController.ts
+++ src/controllers/redirectController.ts
@@ -4,9 +4,9 @@ export default class RedirectController {
   static PATHS = {
     sept21: '/sep21',
   }
-  static sept21Url = 'https://ginger-flood-c72.notion.site/The-2023-Celebration-of-Rachel-s-32nd-Birthday-Also-Known-As-Halloween-in-September-a17c5537f8594c30bb301a48c988f7ec'
+  static sep21Url = 'https://ginger-flood-c72.notion.site/The-2023-Celebration-of-Rachel-s-32nd-Birthday-Also-Known-As-Halloween-in-September-a17c5537f8594c30bb301a48c988f7ec'
 
   static async sept21 (_req: Request, res: Response): Promise<void> {
-    res.redirect(RedirectController.sept21Url)
+    res.redirect(RedirectController.sep21Url)
   }
 }
