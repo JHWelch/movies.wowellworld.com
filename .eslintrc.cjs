2d5a42074f21551306a06a0df1f6cf19117fc1e2
Jordan Welch <jordanhwelch@gmail.com>
2023-12-23 19:00:14 -0600

    Install vue eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-recommended',
   ],
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
