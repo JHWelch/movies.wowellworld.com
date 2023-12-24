63818c63629bae70fa4c28db4818347c5f9f6a8a
Jordan Welch <jordanhwelch@gmail.com>
2023-12-24 08:37:53 -0600

    Migrate LoadingAnimation

---
+++ client/src/components/LoadingAnimation.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="flex items-center w-full h-48 mt-10 mb-5">
+    <div class="mx-auto loading">
+      <div />
+
+      <div />
+
+      <div />
+    </div>
+  </div>
+</template>
