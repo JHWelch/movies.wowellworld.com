9eb423cfce4b6d78f4f861551196dd6a1cb9308b
Jordan Welch <jordanhwelch@gmail.com>
2023-12-23 18:41:52 -0600

    Install vue router

---
+++ client/src/router.ts
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import About from './pages/About.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: HelloWorld,
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
