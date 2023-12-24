775253a7020730365bb55d6826848b5d8762e8b1
Jordan Welch <jordanhwelch@gmail.com>
2023-12-24 08:45:43 -0600

    migrate sectionTitle

---
+++ client/src/components/SectionTitle.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  sectionTitle: string
+}>()
+
+const id = props.sectionTitle
+  .replace(/\s+/g, '-')
+  .toLowerCase()
+</script>
+
+<template>
+  <div>
+    <div
+      v-if="sectionTitle"
+      class="flex justify-center w-full"
+    >
+      <h2
+        :id="id"
+        class="relative flex items-center justify-between w-full max-w-5xl text-2xl italic text-center"
+      >
+        <span class="flex-1 ml-4 mr-12 border-2 border-b border-violet-300" />
+
+        <span v-text="sectionTitle" />
+
+        <span class="flex-1 ml-12 mr-4 border-2 border-b border-violet-300" />
+      </h2>
+    </div>
+
+    <div
+      v-else
+      class="flex justify-center w-full"
+    >
+      <span class="relative w-full max-w-5xl mx-4 text-2xl text-center border-2 border-b border-violet-300" />
+    </div>
+  </div>
+</template>
