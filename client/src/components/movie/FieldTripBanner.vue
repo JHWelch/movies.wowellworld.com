64ea059cf28420071ec4f8046101bf992d1a90ac
Jordan Welch <jordanhwelch@gmail.com>
2023-12-28 08:49:31 -0600

    Update field trip banner to use DTOs

--- client/src/components/movie/FieldTripBanner.vue
+++ client/src/components/movie/FieldTripBanner.vue
@@ -1,20 +1,20 @@
 <script lang="ts" setup>
+import { MovieDto, WeekDto } from '../../../../shared/dtos'
 import IconTicket from '../../icons/IconTicket.vue'
 
 defineProps<{
-  movie: any
-  showEventDetails: boolean
-  week: any
+  movie: MovieDto
+  week: WeekDto
 }>()
 </script>
 
 <template>
   <a
     v-if="movie.isFieldTrip"
-    :href="movie.showingUrl"
+    :href="movie.showingUrl ?? undefined"
     class="absolute inset-0 flex items-center justify-between w-full h-12 p-3 font-medium text-center text-white text-md bg-opacity-80 bg-violet-500"
   >
-    <span x-text="movie.theaterName" />
+    <span v-text="movie.theaterName" />
 
     <IconTicket class="w-6 h-6 ml-2 -rotate-45" />
   </a>
