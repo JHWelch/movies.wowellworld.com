c714ef9a34f0fd898a05524e6017fa699d83ab85
Jordan Welch <jordanhwelch@gmail.com>
2023-12-28 20:17:03 -0600

    Toggle Button color

--- client/src/components/navbar/NavButton.vue
+++ client/src/components/navbar/NavButton.vue
@@ -7,15 +7,14 @@ defineProps<{
 }>()
 </script>
 
-<!-- <%= route === path
-        ? 'text-white bg-violet-600'
-        : 'text-gray-800 hover:bg-violet-300'
-      %>" -->
-
 <template>
   <router-link
     :to="route"
     class="h-full py-2 px-4 font-medium flex flex-col justify-center items-center grow sm:grow-0"
+    :class="{
+      'text-white bg-violet-600': route === $route.path,
+      'text-gray-800 hover:bg-violet-300': route !== $route.path,
+    }"
   >
     {{ text }}
   </router-link>
