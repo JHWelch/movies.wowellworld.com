792a1fdcb230f61f3303d3bbf03ed4275eb0e3dd
Jordan Welch <jordanhwelch@gmail.com>
2024-01-06 22:15:42 -0600

    Add transition

--- client/src/components/Notifications.vue
+++ client/src/components/Notifications.vue
@@ -17,32 +17,41 @@ const icon = {
 </script>
 
 <template>
-  <div
-    v-if="notifications.show()"
-    data-testid="notifications"
-    class="fixed top-0 right-0 py-4 pl-4 pr-6 m-4 rounded-lg shadow-lg text-white z-50 flex space-x-4 justify-center items-center"
-    :class="{
-      'bg-green-500': notifications.type === 'success',
-      'bg-red-500': notifications.type === 'error',
-      'bg-yellow-500': notifications.type === 'warning',
-      'bg-blue-500': notifications.type === 'info',
-    }"
+  <Transition
+    enter-active-class="transition ease-out duration-400"
+    enter-from-class="opacity-0"
+    enter-to-class="opacity-100"
+    leave-active-class="transition ease-in duration-300"
+    leave-from-class="opacity-100"
+    leave-to-class="opacity-0"
   >
-    <component
-      :is="icon[notifications.type || 'info']"
-      class="w-6 h-6"
-    />
-
-    <p>
-      {{ notifications.message }}
-    </p>
-
-    <button
-      class="absolute top-0 right-0 p-2"
-      data-testid="notifications-close"
-      @click="notifications.close()"
+    <div
+      v-if="notifications.show()"
+      data-testid="notifications"
+      class="fixed top-0 right-0 py-4 pl-4 pr-6 m-4 rounded-lg shadow-lg text-white z-50 flex space-x-4 justify-center items-center"
+      :class="{
+        'bg-green-500': notifications.type === 'success',
+        'bg-red-500': notifications.type === 'error',
+        'bg-yellow-500': notifications.type === 'warning',
+        'bg-blue-500': notifications.type === 'info',
+      }"
     >
-      <XMarkIcon class="w-4 h-4" />
-    </button>
-  </div>
+      <component
+        :is="icon[notifications.type || 'info']"
+        class="w-6 h-6"
+      />
+
+      <p>
+        {{ notifications.message }}
+      </p>
+
+      <button
+        class="absolute top-0 right-0 p-2"
+        data-testid="notifications-close"
+        @click="notifications.close()"
+      >
+        <XMarkIcon class="w-4 h-4" />
+      </button>
+    </div>
+  </Transition>
 </template>
