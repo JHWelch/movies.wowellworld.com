500069100a08d9ea1102f8172aeb5d85ba0205d8
Jordan Welch <jordan@shelterluv.com>
2024-01-10 17:40:23 -0600

    Add withMessage helper

---
+++ server/src/helpers/messageBuilder.ts
@@ -0,0 +1,11 @@
+import { NotificationType } from '../../../shared/notifications'
+
+const withMessage = (
+  route: string,
+  message: string,
+  type: NotificationType = 'success',
+) => route + '?message=' + encodeURIComponent(message) + '&type=' + type
+
+export {
+  withMessage,
+}
