b7f18e576e35133fd07b2ea23a801bf505368698
Jordan Welch <jordanhwelch@gmail.com>
2024-08-10 12:48:31 -0500

    Make director optional

--- client/src/components/__tests__/MovieItem.spec.ts
+++ client/src/components/__tests__/MovieItem.spec.ts
@@ -5,6 +5,7 @@ import { mount } from '@vue/test-utils'
 import MovieItem from '../MovieItem.vue'
 import WeekFactory from '../../__tests__/utils/factories/weekFactory'
 import MovieFactory from '../../__tests__/utils/factories/movieFactory'
+import IconLabel from '../IconLabel.vue'
 
 
 it('has all movie details', () => {
@@ -18,10 +19,30 @@ it('has all movie details', () => {
     },
   })
 
+  expect(wrapper.findComponent(IconLabel).exists()).toBe(true)
   expect(wrapper.text()).toContain(movie.title)
   expect(wrapper.text()).toContain(movie.director)
   expect(wrapper.text()).toContain(movie.displayLength)
   expect(wrapper.text()).toContain(movie.year)
   // Cannot figure out why this won't work
   // expect(wrapper.text()).toContain(movie.time)
+})
+
+it('does not show event details when not provided', () => {
+  const movie = new MovieFactory().build({
+    director: null,
+    year: null,
+    displayLength: null,
+  })
+  const week = new WeekFactory().withMovies([movie]).build()
+  const wrapper = mount(MovieItem, {
+    props: {
+      movie: movie,
+      showEventDetails: true,
+      week: week,
+    },
+  })
+
+  expect(wrapper.findComponent(IconLabel).exists()).toBe(false)
+  expect(wrapper.find('[data-testid="movie-director"]').exists()).toBe(false)
 })
