a6715b0d9c82c54319d43eb6163defe5b46100b0
Jordan Welch <jordanhwelch@gmail.com>
2024-08-24 18:14:09 -0500

    Update config for client absolute paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,26 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    tsconfigPaths(),
+    vue(),
+  ],
   build: {
     manifest: true,
     outDir: '../built/public',
     rollupOptions: {
       input: 'src/main.ts',
+    },
+  },
+  resolve: {
+    alias: {
+      '@client/': new URL('./src/', import.meta.url).pathname,
+      '@components/': new URL('./src/components/', import.meta.url).pathname,
+      '@pages/': new URL('./src/pages/', import.meta.url).pathname,
+      '@shared/': new URL('../shared/', import.meta.url).pathname,
     },
   },
 })
