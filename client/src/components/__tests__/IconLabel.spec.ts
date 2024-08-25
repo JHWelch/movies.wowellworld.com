ae9d53e83c3f7b69ae41da16e3cb9e960ca8d2b5
Jordan Welch <jordanhwelch@gmail.com>
2024-08-25 11:16:50 -0500

    Fix test

--- client/src/components/__tests__/IconLabel.spec.ts
+++ client/src/components/__tests__/IconLabel.spec.ts
@@ -3,17 +3,20 @@
 import { mount } from '@vue/test-utils'
 import IconLabel from '@components/IconLabel.vue'
 import { describe, expect, it } from 'vitest'
+import DynamicHeroIcon from '@components/DynamicHeroIcon.vue'
 
 describe('Default arguments', () => {
   it('renders a label with an icon', async () => {
     const wrapper = mount(IconLabel, {
       props: {
-        icon: 'Calendar',
+        icon: 'CalendarIcon',
         label: 'Movie Director',
       },
     })
 
+    const icon = wrapper.findComponent(DynamicHeroIcon)
     expect(wrapper.text()).toContain('Movie Director')
-    // expect(wrapper.find('svg').exists()).toBe(true)
+    expect(icon.exists()).toBe(true)
+    expect(icon.props('name')).toBe('CalendarIcon')
   })
 })
