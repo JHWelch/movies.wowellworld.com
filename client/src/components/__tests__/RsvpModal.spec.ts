5900b5dd372ec1dc7747f10bf535fbc7f6364cd1
Jordan Welch <jordanhwelch@gmail.com>
2024-10-04 20:16:59 -0500

    test existing functionality

--- client/src/components/__tests__/RsvpModal.spec.ts
+++ client/src/components/__tests__/RsvpModal.spec.ts
@@ -1,10 +1,11 @@
 /** @vitest-environment jsdom */
 
-import { describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import RsvpModal from '@components/RsvpModal.vue'
 import WeekFactory from '@tests/utils/factories/weekFactory'
 import { rsvpModal } from '@client/state/modalState'
+import { fireConfetti } from '@client/utilities/confetti'
 
 describe('nothing input', () => {
   it('disables the submit button', () => {
@@ -25,5 +26,61 @@ describe('only name input', () => {
     await wrapper.find('[data-testid="input-name"]').setValue('John Doe')
 
     expect(wrapper.find('[data-testid="rsvp-button"]').attributes('disabled')).toBe(undefined)
+  })
+})
+
+describe('rsvp submit', () => {
+  beforeEach(() => {
+    fetch.doMock()
+    vi.mock(import('@client/utilities/confetti'), () => ({
+      fireConfetti: vi.fn(),
+    }))
+  })
+
+  it('calls api with the correct data', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}))
+    const week = new WeekFactory().build({
+      weekId: '2020-01-01',
+    })
+    rsvpModal.open(week)
+    const wrapper = mount(RsvpModal)
+    await wrapper.find('[data-testid="input-name"]').setValue('John Doe')
+    await wrapper.find('[data-testid="input-email"]').setValue('jdoe@example.com')
+
+    await wrapper.find('[data-testid="rsvp-button"]').trigger('click')
+
+    expect(fetch.requests().length).toEqual(1)
+    const request = fetch.requests()[0]
+    expect(request.method).toEqual('POST')
+    expect(request.url).toEqual('/api/weeks/2020-01-01/rsvp')
+    expect(JSON.parse(request.body)).toEqual({
+      name: 'John Doe',
+      email: 'jdoe@example.com',
+      plusOne: false,
+    })
+  })
+
+  it('closes the modal', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}))
+    const week = new WeekFactory().build()
+    rsvpModal.open(week)
+    const wrapper = mount(RsvpModal)
+    await wrapper.find('[data-testid="input-name"]').setValue('John Doe')
+
+    await wrapper.find('[data-testid="rsvp-button"]').trigger('click')
+
+    expect(rsvpModal.show).toBe(false)
+  })
+
+  it('fires confetti', async () => {
+    fetch.mockResponseOnce(JSON.stringify({}))
+    const week = new WeekFactory().build()
+    rsvpModal.open(week)
+    const wrapper = mount(RsvpModal)
+    await wrapper.find('[data-testid="input-name"]').setValue('John Doe')
+
+    await wrapper.find('[data-testid="rsvp-button"]').trigger('click')
+
+    expect(fireConfetti).toHaveBeenCalled()
   })
 })
