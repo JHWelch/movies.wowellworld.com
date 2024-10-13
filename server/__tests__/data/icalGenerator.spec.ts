5412918bbf66c0124e4123ec63f79e2a7d78d478
Jordan Welch <jordanhwelch@gmail.com>
2024-10-13 12:40:55 -0500

    Add movie information to calendar event body

--- server/__tests__/data/icalGenerator.spec.ts
+++ server/__tests__/data/icalGenerator.spec.ts
@@ -12,10 +12,28 @@ it('can generate an ical file from a week', () => {
   const week = new WeekFactory().make()
   week.movies = [
     new MovieFactory().make({
+      title: 'Labyrinth',
+      director: 'Jim Henson',
+      year:1986,
+      url: 'https://example.com/labyrinth',
+      tmdbId: 1234,
+      posterPath: '/path/to/labyrinth.jpg',
+      notionId: 'notionId1',
+      theaterName: 'Theater',
+      showingUrl: 'https://cal.example.com/labyrinth',
       time: '6:00 PM',
       length: 105,
     }),
     new MovieFactory().make({
+      title: 'The NeverEnding Story',
+      director: 'Wolfgang Petersen',
+      year: 1984,
+      url: 'https://example.com/neverending-story',
+      tmdbId: 1234,
+      posterPath: '/path/to/neverending_story.jpg',
+      notionId: 'notionId2',
+      theaterName: 'Theater',
+      showingUrl: 'https://cal.example.com/neverending-story',
       time: '8:00 PM',
     }),
   ]
--- server/src/data/icalGenerator.ts
+++ server/src/data/icalGenerator.ts
@@ -2,13 +2,21 @@
 import { Week } from '@server/models/week'
 import * as ics from 'ics'
 
+const description = (week: Week): string => 'Come one, come all (okay maybe not all) to the Welch-Cowell/Cowell-Welch/Wowell household for a recurring Thursday Night Movie Club!'
+  + '\n\n'
+  + 'This week\'s theme is ' + week.theme + '.'
+  + '\n\n'
+  + week.movies
+    .map(movie => `${movie.time} - ${movie.title} (${movie.year})`)
+    .join('\n')
+
 export const icalGenerator = (week: Week): string | undefined => {
   const { error, value } = ics.createEvent({
     uid: week.dateString + '@movies.wowellworld.com',
     start: week.startTime.toJSDate().getTime(),
     duration: { minutes: week.totalLength },
     title: 'Movie Nights: ' + week.theme,
-    description: 'Come one, come all (okay maybe not all) to the Welch-Cowell/Cowell-Welch/Wowell household for a recurring Thursday Night Movie Club!',
+    description: description(week),
     location: '2111 W Argyle St. Chicago, IL 60625',
     url: 'https://movies.wowellworld.com/',
     status: 'CONFIRMED',
