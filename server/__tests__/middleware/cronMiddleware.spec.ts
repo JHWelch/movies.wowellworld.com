71c3e21d3255303c89803c64100090c4b5cbccee
Jordan Welch <jordanhwelch@gmail.com>
2024-10-30 19:40:39 -0500

    use 401

--- server/__tests__/middleware/cronMiddleware.spec.ts
+++ server/__tests__/middleware/cronMiddleware.spec.ts
@@ -23,7 +23,7 @@ describe('Request has no headers', () => {
     }
     cronMiddleware(req, res, nextFunction)
 
-    expect(res.status).toBeCalledWith(403)
+    expect(res.status).toBeCalledWith(401)
     expect(res.json).toBeCalledWith(expectedResponse)
   })
 })
@@ -41,7 +41,7 @@ describe('Request missing X-Appengine-Cron header', () => {
     }
     cronMiddleware(req, res, nextFunction)
 
-    expect(res.status).toBeCalledWith(403)
+    expect(res.status).toBeCalledWith(401)
     expect(res.json).toBeCalledWith(expectedResponse)
   })
 })
