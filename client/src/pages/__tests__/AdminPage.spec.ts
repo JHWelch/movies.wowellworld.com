cd89ccde9f85cee2d0cc01ff99c87e8037565e37
Jordan Welch <jordanhwelch@gmail.com>
2024-10-30 21:55:01 -0500

    Test everything displaying properly

--- client/src/pages/__tests__/AdminPage.spec.ts
+++ client/src/pages/__tests__/AdminPage.spec.ts
@@ -1,43 +1,89 @@
 /** @vitest-environment jsdom */
 
 import { beforeEach, describe, expect, it } from 'vitest'
-import { mount } from '@vue/test-utils'
+import { mount, VueWrapper } from '@vue/test-utils'
 import AdminPage from '@pages/AdminPage.vue'
-import { nextTick } from 'vue'
+
+let wrapper: VueWrapper
 
 describe('unauthed page', () => {
   it('shows password form', () => {
-    const wrapper = mount(AdminPage)
+    wrapper = mount(AdminPage)
 
     expect(wrapper.byTestId('unlock-button').exists()).toBe(true)
     expect(wrapper.byTestId('input-password').exists()).toBe(true)
   })
 
   it('does not show admin content', () => {
-    const wrapper = mount(AdminPage)
+    wrapper = mount(AdminPage)
 
     expect(wrapper.text()).not.toContain('Sync Weeks')
   })
 
-  describe('correct password', () => {
-    beforeEach(() => {
-      window.fetch.mockResponseOnce(JSON.stringify({
-        updatedWeeks: 0,
-        previousLastUpdated: '2021-01-01T00:00:00.000Z',
-        newLastUpdated: '2021-01-01T00:00:00.000Z',
-        tmdbMoviesSynced: [],
-      }))
-    })
+})
 
-    it('shows admin content', async () => {
-      const wrapper = mount(AdminPage)
+describe('authentication problems', () => {
+  describe('401', () => {
+    beforeEach(async () => {
+      window.fetch.mockResponseOnce(JSON.stringify({
+        error: 'Something went wrong authenticating you',
+      }), {
+        status: 401,
+      })
+      wrapper = mount(AdminPage)
       await wrapper.byTestId('input-password').setValue('password')
       await wrapper.byTestId('unlock-button').trigger('click')
-      await nextTick()
-      await nextTick()
-      await nextTick()
-
-      expect(wrapper.text()).toContain('Sync Weeks')
     })
+
+    it('does not shows admin content', async () => {
+      expect(wrapper.text()).not.toContain('Sync Weeks')
+      expect(wrapper.text()).toContain('Something went wrong authenticating you')
+    })
+  })
+
+  describe('403', () => {
+    beforeEach(async () => {
+      window.fetch.mockResponseOnce(JSON.stringify({
+        error: 'Password incorrect',
+      }), {
+        status: 403,
+      })
+      wrapper = mount(AdminPage)
+      await wrapper.byTestId('input-password').setValue('password')
+      await wrapper.byTestId('unlock-button').trigger('click')
+    })
+
+    it('does not shows admin content', async () => {
+      expect(wrapper.text()).not.toContain('Sync Weeks')
+      expect(wrapper.text()).toContain('Password incorrect')
+    })
+  })
+})
+
+describe('correct password - logged in', () => {
+  beforeEach(async () => {
+    window.fetch.mockResponseOnce(JSON.stringify({
+      updatedWeeks: 15,
+      previousLastUpdated: '2021-01-01T00:00:00.000Z',
+      newLastUpdated: '2021-01-01T00:00:00.000Z',
+      tmdbMoviesSynced: Array.from({ length: 32 }, () => {}),
+    }))
+
+    wrapper = mount(AdminPage)
+    await wrapper.byTestId('input-password').setValue('password')
+    await wrapper.byTestId('unlock-button').trigger('click')
+  })
+
+  it('shows admin content', async () => {
+    expect(wrapper.text()).toContain('Sync Weeks')
+  })
+
+  it('shows updated weeks metadata', async () => {
+    expect(wrapper.text()).toContain('15')
+    expect(wrapper.text())
+      .toContain(new Date('2021-01-01T00:00:00.000Z').toLocaleString())
+    expect(wrapper.text())
+      .toContain(new Date('2021-01-01T00:00:00.000Z').toLocaleString())
+    expect(wrapper.text()).toContain('32')
   })
 })
