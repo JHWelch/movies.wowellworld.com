f5bc5dc886e373eea776d289a9219d39a4678e55
Jordan Welch <jordanhwelch@gmail.com>
2024-11-17 20:53:55 -0600

    Move rules to eslint 3.0.0

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,22 +11,5 @@ export default [
       'no-restricted-imports': ['error', {
         patterns: ['.*'], // Disable all relative imports
       }],
-      '@stylistic/padding-line-between-statements': [
-        'error',
-        { blankLine: 'always', prev: '*', next: 'return' },
-      ],
-      '@stylistic/member-delimiter-style': [
-        'error',
-        {
-          multiline: {
-            delimiter: 'none',
-          },
-          singleline: {
-            delimiter: 'comma',
-            requireLast: false,
-          },
-          multilineDetection: 'brackets',
-        },
-      ],
     },
   }]
