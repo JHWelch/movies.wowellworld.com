0f6eebe7f42a1a9b8a8874fb62b63bea40a10c8b
Jordan Welch <jordanhwelch@gmail.com>
2024-12-07 22:04:20 -0600

    backfill WeekItem tests

--- client/src/components/__tests__/WeekItem.spec.ts
+++ client/src/components/__tests__/WeekItem.spec.ts
@@ -1,9 +1,10 @@
 /** @vitest-environment jsdom */
 
-import { expect, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { mount } from '@vue/test-utils'
 import WeekItem from '@components/WeekItem.vue'
 import WeekFactory from '@tests/utils/factories/weekFactory'
+import SkippedBanner from '@components/SkippedBanner.vue'
 
 it('has id of the week\'s slug', () => {
   const week = new WeekFactory().build({ slug: 'week-slug' })
@@ -15,4 +16,45 @@ it('has id of the week\'s slug', () => {
   })
 
   expect(wrapper.find('#week-slug').exists()).toBe(true)
+})
+
+it('has all week details', () => {
+  const week = new WeekFactory().build()
+  const wrapper = mount(WeekItem, {
+    props: {
+      week: week,
+      showEventDetails: true,
+    },
+  })
+
+  expect(wrapper.text()).toContain(week.theme)
+  expect(wrapper.text()).toContain(week.date)
+})
+
+describe('is not skipped', () => {
+  it('does not show skipped banner', () => {
+    const week = new WeekFactory().build()
+    const wrapper = mount(WeekItem, {
+      props: {
+        week: week,
+        showEventDetails: true,
+      },
+    })
+
+    expect(wrapper.findComponent(SkippedBanner).exists()).toBe(false)
+  })
+})
+
+describe('is skipped', () => {
+  it('shows skipped banner', () => {
+    const week = new WeekFactory().build({ isSkipped: true })
+    const wrapper = mount(WeekItem, {
+      props: {
+        week: week,
+        showEventDetails: true,
+      },
+    })
+
+    expect(wrapper.findComponent(SkippedBanner).exists()).toBe(true)
+  })
 })
