6f5685a574970923353e768ac272ea31618aed56
Jordan Welch <jordanhwelch@gmail.com>
2025-05-10 20:53:46 -0500

    Simplify config

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -12,33 +12,35 @@ export default class Config {
   tmdbApiKey: string
 
   constructor () {
-    this.adminEmail = this.requiredVariable('ADMIN_EMAIL')
-    this.apiPassword = this.requiredVariable('API_PASSWORD')
-    this.appUrl = this.requiredVariable('APP_URL')
-    this.calendarUrl = this.requiredVariable('CALENDAR_URL')
-    this.googleCloudProject = this.requiredVariable('GOOGLE_CLOUD_PROJECT')
-    this.nodeEnv = this.optionalVariable('NODE_ENV', 'development')
-    this.notionMovieDatabaseId = this.requiredVariable('NOTION_MOVIE_DATABASE_ID')
-    this.notionEventDatabaseId = this.requiredVariable('NOTION_WEEK_DATABASE_ID')
-    this.notionToken = this.requiredVariable('NOTION_TOKEN')
-    this.port = parseInt(this.optionalVariable('PORT', '8080'))
-    this.tmdbApiKey = this.requiredVariable('TMDB_READ_KEY')
+    this.adminEmail = requiredVariable('ADMIN_EMAIL')
+    this.apiPassword = requiredVariable('API_PASSWORD')
+    this.appUrl = requiredVariable('APP_URL')
+    this.calendarUrl = requiredVariable('CALENDAR_URL')
+    this.googleCloudProject = requiredVariable('GOOGLE_CLOUD_PROJECT')
+    this.notionMovieDatabaseId = requiredVariable('NOTION_MOVIE_DATABASE_ID')
+    this.notionEventDatabaseId = requiredVariable('NOTION_WEEK_DATABASE_ID')
+    this.notionToken = requiredVariable('NOTION_TOKEN')
+    this.tmdbApiKey = requiredVariable('TMDB_READ_KEY')
+
+    this.nodeEnv = optionalVariable('NODE_ENV', 'development')
+    this.port = parseInt(optionalVariable('PORT', '8080'))
   }
 
   get isProduction (): boolean {
     return this.nodeEnv === 'production'
   }
-
-  requiredVariable (name: string): string {
-    const value = process.env[name]
-    if (value == null) {
-      throw new Error(`${name} is missing`)
-    }
-
-    return value
-  }
-
-  optionalVariable (name: string, fallback: string): string {
-    return process.env[name] ?? fallback
-  }
 }
+
+const requiredVariable = (name: string): string => {
+  const value = process.env[name]
+  if (value == null) {
+    throw new Error(`${name} is missing`)
+  }
+
+  return value
+}
+
+const optionalVariable = (
+  name: string,
+  fallback: string
+): string => process.env[name] ?? fallback
