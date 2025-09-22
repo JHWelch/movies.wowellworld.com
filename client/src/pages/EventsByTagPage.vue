3351bafb7558ca2e887aaca760dbadbe2df72243
Jordan Welch <jordanhwelch@gmail.com>
2025-09-21 20:26:14 -0400

    Add description

--- client/src/pages/EventsByTagPage.vue
+++ client/src/pages/EventsByTagPage.vue
@@ -2,6 +2,7 @@
 import EventList from '@components/EventList.vue'
 import { useRouter } from 'vue-router'
 import { tags } from '@client/data/tags'
+import SectionTitle from '@client/components/SectionTitle.vue'
 
 const props = defineProps<{
   tag: string
@@ -10,6 +11,7 @@ const router = useRouter()
 const {
   query,
   display,
+  description,
 } = tags?.[props.tag.toLowerCase()] || {
   query: props.tag,
   display: props.tag,
@@ -17,12 +19,21 @@ const {
 </script>
 
 <template>
-  <div class="mt-10">
-    <EventList
-      :fetch-url="`/api/events?tag=${query}`"
-      :show-event-details="true"
-      :section-titles="{ 0: display }"
-      :on-empty="() => router.replace('/404')"
-    />
+  <div class="flex flex-col items-center w-full pt-20">
+    <SectionTitle :section-title="display" />
+    <div class="max-w-4xl">
+      <section
+        v-if="description"
+        class="px-4 py-8 mt-4 space-y-5 leading-7 min-[896px]:rounded-xl md:px-8 md:mt-8"
+      >
+        <p>{{ description }}</p>
+      </section>
+    </div>
   </div>
+
+  <EventList
+    :fetch-url="`/api/events?tag=${query}`"
+    :show-event-details="true"
+    :on-empty="() => router.replace('/404')"
+  />
 </template>
