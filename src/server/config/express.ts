8461c900894a2c41a68efc325a5ed281f43c643b
Jordan Welch <jordanhwelch@gmail.com>
2026-01-19 15:35:47 -0600

    Remove EJS & static from express setup

--- src/server/config/express.ts
+++ src/server/config/express.ts
@@ -5,8 +5,6 @@ import directoryPath from '@server/helpers/directoryPath'
 export default function setupExpress (): express.Express {
   const dirname = directoryPath()
   const app = express()
-  app.set('view engine', 'ejs')
-  app.use('/', express.static(`${dirname}/../../../public`))
   app.use(
     '/made-with',
     express.static(`${dirname}/../../../node_modules/@jhwelch/made-with`),
