960be506eb6cdfa9f9e1433bc4c8c0614bf5fc38
Jordan Welch <jordanhwelch@gmail.com>
2026-03-21 10:49:31 -0500

    use `@tailwindcss/vite` over `@tailwindcss/postcss`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     vue(),
   ],
   resolve: {
