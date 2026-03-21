9ae9425819051213b09875625f1e91cab7f0c1eb
Jordan Welch <jordanhwelch@gmail.com>
2026-03-21 10:13:38 -0500

    Focus states and hover states

--- src/client/components/navbar/NavButton.vue
+++ src/client/components/navbar/NavButton.vue
@@ -8,7 +8,7 @@ defineProps<{
 <template>
   <router-link
     :to="route"
-    class="flex flex-col text-sm items-center justify-center h-full p-2 leading-5 text-center md:text-xl grow sm:grow-0"
+    class="flex flex-col items-center justify-center h-full p-2 text-sm leading-5 text-center md:text-xl grow sm:grow-0 hover:bg-primary-dark hover:text-dark focus:bg-primary-dark focus:text-dark focus:outline-none focus:ring-2 focus:ring-offset-4 focus:ring-offset-dark focus:ring-primary-dark"
     :class="{
       'font-bold': route === $route.path,
       'font-medium ': route !== $route.path,
